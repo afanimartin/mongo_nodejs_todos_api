467e727fa2b1990d0d1e5916443a4db1684cec08
Martin Afani <martin@orderly.ai>
2021-10-27 21:30:20 +0200

    initialize app

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+
+const connectToDB = require("./lib/db/connect");
+const router = require("./lib/routes/routes");
+const notFound = require("./lib/middleware/notFound");
+const errorHandler = require("./lib/middleware/errorHandler")
+
+app.use(express.json());
+app.use("/tasks", router);
+app.use(notFound);
+app.use(errorHandler)
+
+app.use(cors)
+
+const startApp = async () => {
+  try {
+    await connectToDB(process.env.MONGODB_URI);
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
